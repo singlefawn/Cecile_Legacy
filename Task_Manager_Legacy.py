2f2f83a63fbdefe080dce50ca25b274e5bc91bfd
Spider Fawn Library <141889973+singlefawn@users.noreply.github.com>
2023-09-10 23:06:22 -0400

    Update Task_Manager_Legacy.py
    updated task manager

--- Task_Manager_Legacy.py
+++ Task_Manager_Legacy.py
@@ -1,9 +1,3 @@
-# task_manager.py
-# Julia Task Manager Legacy
-# 1997 admin POD
-
-
-
 import sys
 import os
 import json
@@ -26,7 +20,7 @@ class TaskManager(QMainWindow):
 
         self.create_directory("task")
         self.create_directory(os.path.join("task", "completed"))
-        self.create_directory(os.path.join("task", "trash"))
+        self.create_directory(os.path.join("task", "drafts"))  # Added 'drafts' directory
 
         self.load_existing_tasks()
 
@@ -40,16 +34,18 @@ class TaskManager(QMainWindow):
         self.task_title_input = QLineEdit(self)
         self.task_details_input = QTextEdit(self)
         self.add_task_button = QPushButton("Add Task", self)
-        self.scroll_area = QScrollArea(self)
+        self.view_completed_drafts_button = QPushButton("View Completed & Drafts", self)  # Added button
 
         layout.addWidget(QLabel("Task Title:"))
         layout.addWidget(self.task_title_input)
         layout.addWidget(QLabel("Task Details:"))
         layout.addWidget(self.task_details_input)
         layout.addWidget(self.add_task_button)
+        layout.addWidget(self.view_completed_drafts_button)  # Added button
 
         self.scroll_widget = QWidget()
         self.scroll_layout = QVBoxLayout(self.scroll_widget)
+        self.scroll_area = QScrollArea(self)
         self.scroll_area.setWidget(self.scroll_widget)
         self.scroll_area.setWidgetResizable(True)
         self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
@@ -61,6 +57,7 @@ class TaskManager(QMainWindow):
         self.setCentralWidget(container)
 
         self.add_task_button.clicked.connect(self.add_task)
+        self.view_completed_drafts_button.clicked.connect(self.view_completed_drafts)  # Connect the button
 
     def load_existing_tasks(self):
         task_dir = "task"
@@ -79,7 +76,6 @@ class TaskManager(QMainWindow):
                     task = json.load(f)
                     self.tasks.insert(0, task)
                     self.add_task_title_label(task, at_top=True)
-
 
     def add_task(self):
         title = self.task_title_input.text()
@@ -116,7 +112,7 @@ class TaskManager(QMainWindow):
     def show_task_details(self, task):
         dialog = QDialog(self)
         dialog.setWindowTitle(task["title"])
-        dialog.setGeometry(200, 200, 300, 200)
+        dialog.setGeometry(200, 200, 500, 300)  # Increase width for better text wrapping
 
         layout = QVBoxLayout()
 
@@ -129,71 +125,125 @@ class TaskManager(QMainWindow):
         details_label.setTextInteractionFlags(Qt.TextSelectableByMouse)
         timestamp_label.setTextInteractionFlags(Qt.TextSelectableByMouse)
 
+        # Set word wrap for details_label
+        details_label.setWordWrap(True)
+
         layout.addWidget(title_label)
         layout.addWidget(details_label)
         layout.addWidget(timestamp_label)
 
+        complete_button = QPushButton("Complete", dialog)
+        complete_button.clicked.connect(lambda: self.complete_task(task, dialog))
+
+        draft_button = QPushButton("Draft", dialog)
+        draft_button.clicked.connect(lambda: self.draft_task(task, dialog))
+
         close_button = QPushButton("Close", dialog)
         close_button.clicked.connect(dialog.close)
 
-        delete_button = QPushButton("Delete", dialog)
-        delete_button.clicked.connect(lambda: self.delete_task(task, dialog))
-
-        layout.addWidget(delete_button)
+        layout.addWidget(complete_button)
+        layout.addWidget(draft_button)
         layout.addWidget(close_button)
         dialog.setLayout(layout)
         dialog.exec_()
 
-    def delete_task(self, task, dialog):
-        trash_path = os.path.join("task", "trash", f"{task['title']}.json")
-        original_path = os.path.join("task", f"{task['title']}.json")
+    def complete_task(self, task, dialog):
+        # Handle task completion here
+        completed_filename = os.path.join("task", "completed", f"{task['title']}.json")
+        with open(completed_filename, "w") as f:
+            json.dump(task, f, indent=4)
 
-        shutil.move(original_path, trash_path)
-
-        # Clear all task labels from the viewer
-        for i in reversed(range(self.scroll_layout.count())):
-            widget = self.scroll_layout.itemAt(i).widget()
-            if widget is not None:
-                self.scroll_layout.removeWidget(widget)
-                widget.deleteLater()
-
-        # Reload tasks from directory
-        self.load_existing_tasks()
+        # Remove task from the current view
+        self.remove_task_from_view(task)
 
         dialog.close()
 
+    def draft_task(self, task, dialog):
+        # Handle task drafting here
+        drafts_directory = os.path.join("task", "drafts")
 
-class TaskDetailsDialog(QDialog):
-    def __init__(self, task, parent=None):
-        super().__init__(parent)
-        self.setWindowTitle(task["title"])
-        self.setGeometry(200, 200, 300, 200)
+        # Ensure the drafts directory exists
+        self.create_directory(drafts_directory)
+
+        draft_filename = os.path.join(drafts_directory, f"{task['title']}.json")
+        with open(draft_filename, "w") as f:
+            json.dump(task, f, indent=4)
+
+        # Remove task from the current view
+        self.remove_task_from_view(task)
+
+        dialog.close()
+
+    def remove_task_from_view(self, task):
+        # Remove the task label from the viewer
+        for i in reversed(range(self.scroll_layout.count())):
+            widget = self.scroll_layout.itemAt(i).widget()
+            if widget is not None:
+                label_title = widget.text()
+                if label_title == task["title"]:
+                    self.scroll_layout.removeWidget(widget)
+                    widget.deleteLater()
+                    break
+
+    def view_completed_drafts(self):
+        completed_tasks = self.load_tasks_from_directory("task/completed")
+        draft_tasks = self.load_tasks_from_directory("task/drafts")
+
+        # Create a dialog to display completed and draft tasks
+        dialog = QDialog(self)
+        dialog.setWindowTitle("Completed & Draft Tasks")
+        dialog.setGeometry(200, 200, 500, 300)  # Adjust size as needed
 
         layout = QVBoxLayout()
-        layout.addWidget(QLabel("Title: " + task["title"]))
-        layout.addWidget(QLabel("Details: " + task["details"]))
-        layout.addWidget(QLabel("Timestamp: " + task["timestamp"]))
 
-        complete_button = QPushButton("Complete", self)
-        close_button = QPushButton("Close", self)
+        scroll_area_completed = QScrollArea()
+        completed_widget = QWidget()
+        completed_layout = QVBoxLayout(completed_widget)
+        completed_label = QLabel("Completed Tasks:")
 
-        complete_button.clicked.connect(self.complete_task)
-        close_button.clicked.connect(self.close)
+        for task in completed_tasks:
+            completed_layout.addWidget(QLabel(f"- {task['title']}"))
 
-        layout.addWidget(complete_button)
+        scroll_area_completed.setWidget(completed_widget)
+        scroll_area_completed.setWidgetResizable(True)
+
+        scroll_area_drafts = QScrollArea()
+        drafts_widget = QWidget()
+        drafts_layout = QVBoxLayout(drafts_widget)
+        draft_label = QLabel("Draft Tasks:")
+
+        for task in draft_tasks:
+            drafts_layout.addWidget(QLabel(f"- {task['title']}"))
+
+        scroll_area_drafts.setWidget(drafts_widget)
+        scroll_area_drafts.setWidgetResizable(True)
+
+        layout.addWidget(completed_label)
+        layout.addWidget(scroll_area_completed)
+        layout.addWidget(draft_label)
+        layout.addWidget(scroll_area_drafts)
+
+        close_button = QPushButton("Close", dialog)
+        close_button.clicked.connect(dialog.close)
+
         layout.addWidget(close_button)
 
-        self.setLayout(layout)
+        dialog.setLayout(layout)
+        dialog.exec_()
 
-        self.task = task
+    def load_tasks_from_directory(self, directory):
+        tasks = []
 
-    def complete_task(self):
-        # Handle task completion here
-        completed_filename = os.path.join("task", "completed", f"{self.task['title']}.json")
-        with open(completed_filename, "w") as f:
-            json.dump(self.task, f, indent=4)
+        if os.path.exists(directory):
+            for filename in os.listdir(directory):
+                if filename.endswith(".json"):
+                    task_path = os.path.join(directory, filename)
 
-        self.accept()
+                    with open(task_path, "r") as f:
+                        task = json.load(f)
+                        tasks.append(task)
+
+        return tasks
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
